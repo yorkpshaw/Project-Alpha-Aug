826e0c9dbe38578e0f91a824dcbce9cb0db9155c
York <york.p.shaw@gmail.com>
2022-09-12 19:40:09 -0500

    Feature 10 complete

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ from accounts.views import signup
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("signup/", signup, name="signup"),
 ]
