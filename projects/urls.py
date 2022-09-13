ebc9e52e0265539cc3d35b1b04c627e179c23de1
York <york.p.shaw@gmail.com>
2022-09-13 13:09:08 -0500

    Feature 14 complete

--- projects/urls.py
+++ projects/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from projects.views import (
     ProjectListView,
     ProjectDetailView,
+    ProjectCreateView,
 )
 
 urlpatterns = [
     path("", ProjectListView.as_view(), name="list_projects"),
     path("<int:pk>/", ProjectDetailView.as_view(), name="show_project"),
+    path("create/", ProjectCreateView.as_view(), name="create_project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from projects.models import Project
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 
 
 class ProjectListView(LoginRequiredMixin, ListView):
@@ -18,3 +20,16 @@ class ProjectDetailView(LoginRequiredMixin, DetailView):
     model = Project
     template_name = "projects/detail.html"
     context_object_name = "projectdetail"
+
+
+class ProjectCreateView(LoginRequiredMixin, CreateView):
+    model = Project
+    template_name = "projects/create.html"
+    fields = ["name", "description", "members"]
+
+    def get_success_url(self):
+        return reverse_lazy("show_project", args=[self.object.id])
+
+    def form_valid(self, form):
+        form.instance.description = self.request.user
+        return super().form_valid(form)
